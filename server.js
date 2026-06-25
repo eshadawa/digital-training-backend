ef7be61881d11d46961c9074f3052e477712c628
Esha Dawa <eshadawa10@gmail.com>
2026-06-25 16:44:46 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const authRoutes = require("./routes/authRoutes");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ MongoDB Error:", err));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
